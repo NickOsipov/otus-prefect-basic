e3e80457541708523c67bd7f5aad774323dffc6f
NickOsipov <nick.osipov.91@gmail.com>
2026-02-17 07:12:56 +0300

    fix: flow

--- flows/simple.py
+++ flows/simple.py
@@ -1,21 +1,23 @@
 import pandas as pd
-from prefect import flow, task
+from prefect import flow, task, get_run_logger
 
 @task(retries=3, retry_delay_seconds=10)
 def extract_data():
-    print("Starting extract data...")
-    # Имитация получения данных
     return {"data": [10, 20, 30], "status": "ok"}
 
 @task
 def transform_data(raw_data):
     df = pd.DataFrame(raw_data["data"], columns=["value"])
-    print("Data transformed")
     return df["value"].sum()
 
 @flow(name="First Flow Example")
 def main_flow(threshold: int = 40):
+    logger = get_run_logger()
+    logger.info(f"Flow started with threshold: {threshold}")
+    
+    logger.info("Starting extract data...") 
     data = extract_data()
+    logger.info("Data transformed successfully")
     result = transform_data(data)
     if result > threshold:
-        print(f"Alert! Value {result} is above threshold!")
+        logger.warning(f"Alert! Value {result} is above threshold!")
